addd1668411fcd272fb696d30bdfa3993c281bd4
Krishna Tripathi <192.168.9.99kj@gmail.com>
2025-07-18 16:05:38 +0530

    feat(db) : Wrapped the Database connection login in try catch block

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://shreejinetwork702:Xd5OsAeqRz2SLOD4@cluster0.dhjwe.mongodb.net/devTinder"
-  );
+  try {
+    await mongoose.connect(
+      "mongodb+srv://shreejinetwork702:Xd5OsAeqRz2SLOD4@cluster0.dhjwe.mongodb.net/devTinder"
+    );
+  } catch (err) {
+    console.log("Error Occured :", err);
+  }
 };
 
 module.exports = connectDB;
